90d0e1b16332dcf05b1ee7475817c6f758cb7519
assem morsy <assemmorsy@outlook.com>
2023-10-22 12:24:40 +0300

    create match list component

---
+++ components/Match/List.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div v-if="orderedEvents.length > 0" ref="matchCards"
+            class="scroll-smooth overflow-x-hidden overflow-y-scroll max-h-[60vh] px-2 md:px-10 bg-zinc-200 dark:bg-slate-700  rounded-lg ">
+            <template v-for="event in orderedEvents" :key="event.id">
+                <MatchCard :match="event" :id="event.id" :champId="event.league_id" class="my-4" />
+            </template>
+        </div>
+        <div v-else class=" text-lg h-50 flex flex-col justify-center items-center py-10">
+            <Icon name="line-md:alert-circle" class="block text-9xl" />
+            <h3>لا توجد مباريات حاليا</h3>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import IMatchLessDetails from '@/Models/IMatchLessDetails';
+const props = defineProps({
+    matches: {
+        type: Object as PropType<IMatchLessDetails[]>,
+        required: true
+    }
+});
+const matchCards = ref<HTMLElement | null>(null);
+
+const orderedEvents = computed((): (IMatchLessDetails)[] => {
+    return props.matches.sort((a, b) => {
+        let d1 = new Date(a.start_at);
+        let d2 = new Date(b.start_at);
+        return d1.getTime() - d2.getTime();
+    })
+})
+
+
+const offsetOfComingMatch = computed(() => {
+    if (props.matches) {
+        let currentDate = new Date()
+        for (let i = 0; i < orderedEvents.value.length; i++) {
+            if (new Date(orderedEvents.value[i].start_at) >= currentDate) {
+                return i - 1 >= 0 ? i - 1 : 0
+            }
+        }
+        return orderedEvents.value.length - 1;
+    }
+    return null
+})
+watchEffect(() => {
+    if (props.matches && props.matches.length > 0) {
+        if (matchCards.value && offsetOfComingMatch.value) {
+            matchCards.value.scrollTop = (matchCards.value.children[offsetOfComingMatch.value - 1] as HTMLElement).scrollHeight;
+        }
+    }
+})
+</script>
+
+<style scoped></style>
--- pages/championships/[id]/matches.vue
+++ pages/championships/[id]/matches.vue
@@ -1,16 +1,7 @@
 <template>
     <FetchDataWraper :error="error" :pending="pending" class="w-full text-zinc-700 dark:text-slate-50">
         <template #main>
-            <div v-if="orderedEvents.length > 0" ref="matchCards"
-                class="scroll-smooth overflow-x-hidden overflow-y-scroll max-h-[60vh] px-2 md:px-10 bg-zinc-200 dark:bg-slate-700  rounded-lg ">
-                <template v-for="event in orderedEvents" :key="event.id">
-                    <MatchCard :match="event" :id="event.id" :champId="route.params.id" class="my-4" />
-                </template>
-            </div>
-            <div v-else class=" text-lg h-50 flex flex-col justify-center items-center py-10">
-                <Icon name="line-md:alert-circle" class="block text-9xl" />
-                <h3>لا توجد مباريات حاليا</h3>
-            </div>
+            <MatchList :matches="matches" />
         </template>
     </FetchDataWraper>
 </template>
@@ -23,20 +14,13 @@ const route = useRoute();
 const client = useStrapiClient()
 const matches = ref<IMatchLessDetails[]>([]);
 const error = ref<string | null>(null);
-const matchCards = ref<HTMLElement | null>(null);
 const pending = ref(false);
 
 definePageMeta({
     name: "leagueMatches"
 })
 
-const orderedEvents = computed((): (IMatchLessDetails)[] => {
-    return matches.value.sort((a, b) => {
-        let d1 = new Date(a.start_at);
-        let d2 = new Date(b.start_at);
-        return d1.getTime() - d2.getTime();
-    })
-})
+
 const fetchData = () => {
     pending.value = true;
     return client(`/leagues/${route.params.id}/matches`, { method: 'GET' })
@@ -59,25 +43,6 @@ const fetchData = () => {
 onServerPrefetch(fetchData)
 onBeforeMount(fetchData)
 
-const offsetOfComingMatch = computed(() => {
-    if (matches.value) {
-        let currentDate = new Date()
-        for (let i = 0; i < orderedEvents.value.length; i++) {
-            if (new Date(orderedEvents.value[i].start_at) >= currentDate) {
-                return i - 1 >= 0 ? i - 1 : 0
-            }
-        }
-        return orderedEvents.value.length - 1;
-    }
-    return null
-})
-watchEffect(() => {
-    if (matches.value && matches.value.length > 0) {
-        if (matchCards.value && offsetOfComingMatch.value) {
-            matchCards.value.scrollTop = (matchCards.value.children[offsetOfComingMatch.value - 1] as HTMLElement).offsetTop;
-        }
-    }
-})
 
 </script>
 
