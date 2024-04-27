bf5d0f76084f3e29b1073a2a7973355fafd48979
assem morsy <assemmorsy@outlook.com>
2024-04-28 02:37:47 +0300

    add adv banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="container mx-auto my-5 ">
+        <div class="mb-3 w-full relative">
+            <img src="/assets/images/adv-banner.jpg" class="rounded-lg w-full object-cover h-48 object-top" />
+            <h3
+                class="absolute hidden lg:block top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center  text-white lg:text-2xl  ">
+                ورق بلوت زات متاح حاليا بالاسواق
+            </h3>
+            <div class="inline absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <UButton size="lg" class="animate-bounce" target="_blank" to="https://mtjr.at/d_9HVi7ll1">اشترى
+                    الان</UButton>
+            </div>
+
+        </div>
         <HomeSlider />
         <div class="flex justify-around flex-col md:flex-row md:mb-10 ">
             <ClientOnly>
@@ -18,11 +30,13 @@
             <SectionHeader title="خارطة البطولات" icon="i-heroicons-map" />
         </div>
 
-        <img src="https://storage.googleapis.com/qydha_bucket/zatbaloot_assets/btolat_zat.jpg" class="mb-5 rounded-xl" />
+        <img src="https://storage.googleapis.com/qydha_bucket/zatbaloot_assets/btolat_zat.jpg"
+            class="mb-5 rounded-xl" />
         <HomeBlogs />
         <HomeJoinChampions />
 
-        <div dir="ltr" class=" pb-8 text-slate-800 dark:text-white text-center -mb-5 dark:bg-slate-800  overflow-hidden">
+        <div dir="ltr"
+            class=" pb-8 text-slate-800 dark:text-white text-center -mb-5 dark:bg-slate-800  overflow-hidden">
             <ClientOnly>
                 <div :class="[colorMode.preference === 'dark' ? 'stars' : 'stars-light']"></div>
                 <div :class="[colorMode.preference === 'dark' ? 'stars2' : 'stars-light2']"></div>
